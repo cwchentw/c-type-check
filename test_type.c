aee4f2206a6adf504a98e4655e6c972f7995ec30
Michael Chen <cwchen123@gmail.com>
2018-04-25 07:45:48 +0000

    Fix test program

--- test_type.c
+++ test_type.c
@@ -1,31 +1,32 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include "type.h"
 
 int main()
 {
     // bool
     assert(type(({bool flag = true; flag;})) == TYPENAME_BOOL);
-    assert(type_str(({bool flag = true; flag;})) == "bool");
+    assert(strcmp(type_str(({bool flag = true; flag;})), "bool") == 0);
     // char
     assert(type(({char c = 'c'; c; })) == TYPENAME_CHAR);
-    assert(type_str(({char c = 'c'; c; })) == "char");
+    assert(strcmp(type_str(({char c = 'c'; c; })), "char") == 0);
     // short int
     assert(type(({short s = 0; s;})) == TYPENAME_SHORT);
-    assert(type_str(({short s = 0; s;})) == "short");
+    assert(strcmp(type_str(({short s = 0; s;})), "short") == 0);
     // int
     assert(type(0) == TYPENAME_INT);
-    assert(type_str(0) == "int");
+    assert(strcmp(type_str(0), "int") == 0);
     // long int
     assert(type(({long l = 0; l;})) == TYPENAME_LONG);
-    assert(type_str(({long l = 0; l;})) == "long");
+    assert(strcmp(type_str(({long l = 0; l;})), "long") == 0);
     // long long int
     assert(type(({long long ll = 0; ll;})) == TYPENAME_LONG_LONG);
-    assert(type_str(({long long ll = 0; ll;})) == "long long");
+    assert(strcmp(type_str(({long long ll = 0; ll;})), "long long") == 0);
     // char *, i.e. pointer to char
     assert(type("Hello World") == TYPENAME_POINTER_TO_CHAR);
-    assert(type_str("Hello World") == "pointer to char");
+    assert(strcmp(type_str("Hello World"), "pointer to char") == 0);
 
     return 0;
 }
