ced2451760f14649c4df6fcb766fbfc551657e35
Michael Chen <cwchen123@gmail.com>
2018-04-25 08:04:48 +0000

    Clean some code

--- test_type_check.c
+++ test_type_check.c
@@ -9,21 +9,27 @@ int main()
     // bool
     assert(type(({bool flag = true; flag;})) == TYPENAME_BOOL);
     assert(strcmp(type_str(({bool flag = true; flag;})), "bool") == 0);
+    
     // char
     assert(type(({char c = 'c'; c; })) == TYPENAME_CHAR);
     assert(strcmp(type_str(({char c = 'c'; c; })), "char") == 0);
+    
     // short int
     assert(type(({short s = 0; s;})) == TYPENAME_SHORT);
     assert(strcmp(type_str(({short s = 0; s;})), "short") == 0);
+    
     // int
     assert(type(0) == TYPENAME_INT);
     assert(strcmp(type_str(0), "int") == 0);
+    
     // long int
     assert(type(({long l = 0; l;})) == TYPENAME_LONG);
     assert(strcmp(type_str(({long l = 0; l;})), "long") == 0);
+    
     // long long int
     assert(type(({long long ll = 0; ll;})) == TYPENAME_LONG_LONG);
     assert(strcmp(type_str(({long long ll = 0; ll;})), "long long") == 0);
+    
     // char *, i.e. pointer to char
     assert(type("Hello World") == TYPENAME_POINTER_TO_CHAR);
     assert(strcmp(type_str("Hello World"), "pointer to char") == 0);
