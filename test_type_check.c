c77271bd9e9a38162f46758c2aeec303188e4725
Michael Chen <cwchen123@gmail.com>
2018-04-25 08:23:52 +0000

    Update test program

--- test_type_check.c
+++ test_type_check.c
@@ -14,40 +14,61 @@ struct _int {
 int main()
 {
     // bool
-    assert(type(({bool flag = true; flag;})) == TYPENAME_BOOL);
-    assert(strcmp(type_str(({bool flag = true; flag;})), "bool") == 0);
+    {
+        bool flag = true;
+        assert(type(flag) == TYPENAME_BOOL);
+        assert(strcmp(type_str(flag), "bool") == 0);
+    }
 
     // char
-    assert(type(({char c = 'c'; c; })) == TYPENAME_CHAR);
-    assert(strcmp(type_str(({char c = 'c'; c; })), "char") == 0);
+    {
+        char c = 'c';
+        assert(type(c) == TYPENAME_CHAR);
+        assert(strcmp(type_str(c), "char") == 0);
+    }
 
     // short int
-    assert(type(({short s = 0; s;})) == TYPENAME_SHORT);
-    assert(strcmp(type_str(({short s = 0; s;})), "short") == 0);
+    {
+        short s = 0;
+        assert(type(s) == TYPENAME_SHORT);
+        assert(strcmp(type_str(s), "short") == 0);
+    }
 
     // int
-    assert(type(0) == TYPENAME_INT);
-    assert(strcmp(type_str(0), "int") == 0);
+    {
+        assert(type(0) == TYPENAME_INT);
+        assert(strcmp(type_str(0), "int") == 0);
+    }
 
     // long int
-    assert(type(({long l = 0; l;})) == TYPENAME_LONG);
-    assert(strcmp(type_str(({long l = 0; l;})), "long") == 0);
+    {
+        long l = 0;
+        assert(type(l) == TYPENAME_LONG);
+        assert(strcmp(type_str(l), "long") == 0);
+    }
 
     // long long int
-    assert(type(({long long ll = 0; ll;})) == TYPENAME_LONG_LONG);
-    assert(strcmp(type_str(({long long ll = 0; ll;})), "long long") == 0);
+    {
+        long long ll = 0;
+        assert(type(ll) == TYPENAME_LONG_LONG);
+        assert(strcmp(type_str(ll), "long long") == 0);
+    }
 
     // char *, i.e. pointer to char
-    assert(type("Hello World") == TYPENAME_POINTER_TO_CHAR);
-    assert(strcmp(type_str("Hello World"), "pointer to char") == 0);
+    {
+        assert(type("Hello World") == TYPENAME_POINTER_TO_CHAR);
+        assert(strcmp(type_str("Hello World"), "pointer to char") == 0);
+    }
 
     // Pointer to custem struct.
-    Int *obj = malloc(sizeof(Int));
+    {
+        Int *obj = malloc(sizeof(Int));
 
-    assert(type(obj) == TYPENAME_OTHER);
-    assert(strcmp(type_str(obj), "other") == 0);
+        assert(type(obj) == TYPENAME_OTHER);
+        assert(strcmp(type_str(obj), "other") == 0);
 
-    free(obj);
+        free(obj);
+    }
 
     return 0;
 }
